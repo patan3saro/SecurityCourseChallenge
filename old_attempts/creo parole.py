59c842ca188ce2402510a8f2ae0009073549ba36
patan@saro <48280928+patan3saro@users.noreply.github.com>
2025-09-25 17:45:55 +0200

    Delete old_attempts/creo parole.py

--- old_attempts/creo parole.py
+++
@@ -1,9 +0,0 @@
-f = open("C:\\Users\\Utente\\Desktop\\660000_parole_italiane.txt","r",errors='ignore')
-content = f.readlines()
-parole=['']
-for var in content:
-    if(len(var)==9):
-        parole.append(var)
-f=open("C:\\Users\\Utente\\Desktop\\parole_8caratteri_plus.txt","w",errors='ignore')
-f.writelines(parole)
-f.close()
